cf97c3c3f4227d0ef9652a69e788559584267752
Takashi Kanemoto <ttskch@gmail.com>
2024-08-22 04:25:37 +0900

    feat: 🎸 tweak

--- src/CriteriaAwarenessInterface.php
+++ src/CriteriaAwarenessInterface.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace Ttskch\DoctrineOrmCriteria;
 
+use Doctrine\ORM\QueryBuilder;
+use Ttskch\DoctrineOrmCriteria\Criteria\CriteriaInterface;
+
 interface CriteriaAwarenessInterface
 {
+    public function addCriteria(CriteriaInterface $criteria, string $alias): self;
+
+    public function getQueryBuilder(): QueryBuilder;
 }
